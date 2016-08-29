35afeb47d095dcd747bb2ee12a0468987ec0bf9e
Austin Vern Songer <austinvernsonger@outlook.com>
2016-08-29 14:59:40 -0500

    Writing Classes

---
+++ includes/class-old-posts-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://austinvernsonger.github.io
+ * @since      1.0.0
+ *
+ * @package    Old_Posts
+ * @subpackage Old_Posts/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Old_Posts
+ * @subpackage Old_Posts/includes
+ * @author     Austin Vern Songer <austin@austinsonger.com>
+ */
+class Old_Posts_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-old-posts-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://austinvernsonger.github.io
+ * @since      1.0.0
+ *
+ * @package    Old_Posts
+ * @subpackage Old_Posts/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Old_Posts
+ * @subpackage Old_Posts/includes
+ * @author     Austin Vern Songer <austin@austinsonger.com>
+ */
+class Old_Posts_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
